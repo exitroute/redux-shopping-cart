28c869c9447770233a3e1dd6f731cfb6f2add2dd
Ryan O'Shea <ryan.oshea@exitroute.org>
2018-12-05 13:48:34 +0100

    initial commit

---
+++ PLAN.md
@@ -0,0 +1,7 @@
+## React to Redux exercise
+
+[ ] Analyse Tommy's APP - 5  minutes
+[ ] Work through Redux implementation document
+[ ] Create basic file structure
+[ ] Outline structure within file
+
---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+/*
+* Action Types
+*/
+
+// onChange() 
+// update customer form
+// An action for each 
+
+/// "UPDATE" (field, value)
+
+/// "UPDATE_ADDRESS"
+/// "UPDATE_ADDRESS2"
+/// "UPDATE_COUNTRY"
+/// "UPDATE_EMAIL"
+/// "UPDATE_FIRST_NAME"
+/// "UPDATE_LAST_NAME" 
+
+
+// upDataItem()
+// update item in cart
+
+/// these are actions that happen in the Product component
+/// "LIKED"
+/// "AMOUNT_ADD"
+/// "AMOUNT_SUBTRACT"
+
+/// "NAME"
+/// "RELEASE"
+/// "PRICE"
+
